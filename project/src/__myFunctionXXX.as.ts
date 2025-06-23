4c96360c22883d5b4d1fb99388e24facec0d997a
eXory2024 <exory@anio.software>
2025-06-23 20:07:58 +0200

    Update implementation.

--- project/src/__myFunctionXXX.as.ts
+++ project/src/__myFunctionXXX.as.ts
@@ -1,10 +1,13 @@
-import type {EnkoreJSRuntimeContext} from "@anio-software/enkore.js-runtime/v0"
+import type {EnkoreJSRuntimeContextOptions} from "@anio-software/enkore.js-runtime/v0"
+import {createContext} from "@anio-software/enkore.js-runtime"
 
 export async function __implementation(
 //>export function __implementationSync(
-	context: EnkoreJSRuntimeContext
+	options: EnkoreJSRuntimeContextOptions
 ): Promise<number> {
 //>): number {
+	const context = createContext(options, 0)
+
 	context.log("Hello from my lib.")
 
 	context.log.debug("Debug 1", "Debug 2")
